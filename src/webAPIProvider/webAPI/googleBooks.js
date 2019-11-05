8380f904778e14c486cab06452d5d010e128ab55
Shalva Aroshidze <shalva.aroshidze@gmail.com>
2019-11-05 20:55:23 +0400

    add querying volume by id

--- src/webAPIProvider/webAPI/googleBooks.js
+++ src/webAPIProvider/webAPI/googleBooks.js
@@ -1,6 +1,12 @@
 import callApi from '../callApi'
 import { GOOGLE_API_KEY, GOOGLE_BOOKS_BASE_URL } from '../../constants'
 
+export const queryVolumeById = id => callApi({
+  url: `${GOOGLE_BOOKS_BASE_URL}/volumes/${id}`,
+  method: 'get',
+  params: { key: GOOGLE_API_KEY },
+})
+
 export const queryVolumes = ({
   queryString,
   maxResults = 10,
