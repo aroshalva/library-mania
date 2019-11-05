67fdcd4ec2809401906a9a0b59337bf1eb464374
Shalva Aroshidze <shalva.aroshidze@gmail.com>
2019-11-05 19:00:54 +0400

    embedded layout into app. routing works

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -15,45 +15,6 @@ import {
 } from 'semantic-ui-react'
 import mainLibraryIcon from '../mainLibraryIcon.svg'
 
-const FixedMenuLayout = () => (
-  <div>
-    <Menu fixed='top' inverted>
-      <Container>
-        <Menu.Item as='a' header style={{ fontSize: '18px' }}>
-          <Image size='mini' src={mainLibraryIcon} style={{ marginRight: '1.5em' }} />
-
-          Library Mania
-        </Menu.Item>
-
-        <Menu.Item as='a'>Discover</Menu.Item>
-
-        <Menu.Item as='a'>Saved</Menu.Item>
-
-        <Dropdown item simple text='Categories'>
-          <Dropdown.Menu>
-            <Dropdown.Item>Favourites</Dropdown.Item>
-
-            <Dropdown.Item>
-              <i className='dropdown icon' />
-              <span className='text'>My Categories</span>
-              <Dropdown.Menu>
-                <Dropdown.Item>Dummy Category 1</Dropdown.Item>
-                <Dropdown.Item>Dummy Category 1</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown.Item>
-
-            <Dropdown.Item>Manage Categories</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Container>
-    </Menu>
-
-    <Container text style={{ marginTop: '7em' }}>
-      <Header as='h1'>dummy content headear</Header>
-    </Container>
-  </div>
-)
-
 class App extends React.Component {
   render() {
     return (
--- src/features/Layout.jsx
+++ src/features/Layout.jsx
@@ -1,13 +1,71 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
+import {
+  Container,
+  Dropdown,
+  Header,
+  Image,
+  Menu,
+} from 'semantic-ui-react'
+import mainLibraryIcon from '../mainLibraryIcon.svg'
+
+const MenuComponent = () => (
+  <Menu fixed='top' inverted>
+    <Container>
+      <Menu.Item as='a' header style={{ fontSize: '18px' }}>
+        <Image size='mini' src={mainLibraryIcon} style={{ marginRight: '1.5em' }} />
+
+          Library Mania
+      </Menu.Item>
+
+      <Menu.Item as='a'>Discover</Menu.Item>
+
+      <Menu.Item as='a'>Saved</Menu.Item>
+
+      <Dropdown item simple text='Categories'>
+        <Dropdown.Menu>
+          <Dropdown.Item>Favourites</Dropdown.Item>
+
+          <Dropdown.Item>
+            <i className='dropdown icon' />
+            <span className='text'>My Categories</span>
+            <Dropdown.Menu>
+              <Dropdown.Item>Dummy Category 1</Dropdown.Item>
+              <Dropdown.Item>Dummy Category 1</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown.Item>
+
+          <Dropdown.Item>Manage Categories</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </Container>
+  </Menu>
+)
 
 class Layout extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.isActive = this.isActive.bind(this)
+  }
+
+  isActive(path) {
+    return path === this.props.location.pathname
+  }
+
   render() {
     return (
       <div>
-        Layout.
+        <MenuComponent />
+
+        <Container text style={{ marginTop: '7em' }}>
+          <Header as='h1'>dummy content headear</Header>
+
+          {this.props.children}
+        </Container>
       </div>
     )
   }
 }
 
-export default Layout
+export default withRouter(Layout)
