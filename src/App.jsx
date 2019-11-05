8d45fedfc2a9594eaf4d3f281ee729d1d03d505b
Shalva Aroshidze <shalva.aroshidze@gmail.com>
2019-11-05 16:55:18 +0400

    add semantic button to test if semantic ui react setup workds, styles and all.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        hello world
+      <div style={{ padding: '20px' }}>
+        <Button>Click Here</Button>
+        <Button primary>Primary</Button>
       </div>
     )
   }
