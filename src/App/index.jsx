be1de7efc9e9f0c31d069403e22bcf0c85e7a010
Shalva Aroshidze <shalva.aroshidze@gmail.com>
2019-11-05 18:13:18 +0400

    add empty folder structure. Its broken, buts its marely a scaffold

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -16,28 +16,30 @@ const FixedMenuLayout = () => (
   <div>
     <Menu fixed='top' inverted>
       <Container>
-        <Menu.Item as='a' header>
+        <Menu.Item as='a' header style={{ fontSize: '18px' }}>
           <Image size='mini' src={mainLibraryIcon} style={{ marginRight: '1.5em' }} />
 
           Library Mania
         </Menu.Item>
+
         <Menu.Item as='a'>Discover</Menu.Item>
 
-        <Dropdown item simple text='Dropdown'>
+        <Menu.Item as='a'>Saved</Menu.Item>
+
+        <Dropdown item simple text='Categories'>
           <Dropdown.Menu>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Header>Header Item</Dropdown.Header>
+            <Dropdown.Item>Favourites</Dropdown.Item>
+
             <Dropdown.Item>
               <i className='dropdown icon' />
-              <span className='text'>Submenu</span>
+              <span className='text'>My Categories</span>
               <Dropdown.Menu>
-                <Dropdown.Item>List Item</Dropdown.Item>
-                <Dropdown.Item>List Item</Dropdown.Item>
+                <Dropdown.Item>Dummy Category 1</Dropdown.Item>
+                <Dropdown.Item>Dummy Category 1</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
+
+            <Dropdown.Item>Manage Categories</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </Container>
