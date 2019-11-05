3f6a0b9ea41d02a1fb4d6694bd5f9ad263c47d66
Shalva Aroshidze <shalva.aroshidze@gmail.com>
2019-11-05 20:28:22 +0400

    added react redux, hooked up store to react components

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,28 +1,20 @@
 import React from 'react'
 import { SemanticToastContainer } from 'react-semantic-toasts'
+import { Provider } from 'react-redux'
 import 'react-semantic-toasts/styles/react-semantic-alert.css'
 import Router from './Router'
-import {
-  Container,
-  Divider,
-  Dropdown,
-  Grid,
-  Header,
-  Image,
-  List,
-  Menu,
-  Segment,
-} from 'semantic-ui-react'
-import mainLibraryIcon from '../mainLibraryIcon.svg'
+import configureStore from '../../store/configuration'
+
+const store = configureStore()
 
 class App extends React.Component {
   render() {
     return (
-      <React.Fragment>
+      <Provider store={store}>
         <Router />
 
         <SemanticToastContainer position="bottom-right" />
-      </React.Fragment>
+      </Provider>
     )
   }
 }
